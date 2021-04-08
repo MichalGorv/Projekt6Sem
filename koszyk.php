3f8cf08722c5d7f0c8509f2086726860a6d07007
MichalGorv <michal.gorcewicz@wp.pl>
2021-04-08 13:24:33 +0200

    skrypt dodawanie i usuwanie produktów do/z koszyka

---
+++ koszyk.php
@@ -0,0 +1,62 @@
+<?php
+session_start();
+
+if(isset($_POST['koszyk_id'])){
+
+            if($_POST['action'] == 'add'){
+               
+                if(isset($_SESSION['koszyk'])){
+                    $isalreadyExist = 0;
+                    foreach($_SESSION['koszyk'] as $key => $value){
+                        
+                        if($_SESSION['koszyk'][$key]['p_id'] == $_POST['koszyk_id']){
+                            $isalreadyExist++;
+                            $_SESSION['koszyk'][$key]['p_ilosc'] =  $_SESSION['koszyk'][$key]['p_ilosc'] + $_POST['koszyk_ilosc'];
+                        }
+                    }
+                    if($isalreadyExist < 1){
+                        $itemArray = array(
+                            'p_id' => $_POST['koszyk_id'],
+                            'p_nazwa' => $_POST['koszyk_nazwa'], 
+                            'p_cena' => $_POST['koszyk_cena'],
+                            'p_ilosc' => $_POST['koszyk_ilosc'] 
+                        );
+                        $_SESSION['koszyk'][]  = $itemArray;
+                    }
+
+                }else{
+                    $itemArray = array(
+                        'p_id' => $_POST['koszyk_id'],
+                        'p_nazwa' => $_POST['koszyk_nazwa'], 
+                        'p_cena' => $_POST['koszyk_cena'],
+                        'p_ilosc' => $_POST['koszyk_ilosc'] 
+                    );
+                    $_SESSION['koszyk'][]  = $itemArray;
+                }
+           }
+}
+
+if($_POST['action'] == 'remove'){
+    foreach($_SESSION['koszyk'] as $key => $val){
+        if( $val['p_id'] == $_POST['id_to_remove']){
+            unset($_SESSION['koszyk'][$key]);
+        }
+    }
+}
+
+
+if(!empty($_SESSION['koszyk'])){
+    $outputTable = '';
+    $total = 0;
+    $outputTable .= "<table class='table table-bordered'><thead><tr><td>nazwa</td><td>cena</td><td>ilosc</td><td>Usuń</td></tr></thead>";
+    foreach($_SESSION['koszyk'] as $key => $value){
+        $outputTable .= "<tr><td>".$value['p_nazwa']."</td><td>".($value['p_cena'] * $value['p_ilosc']) ."</td><td>".$value['p_ilosc']."</td><td><button id=".$value['p_id']." class='btn btn-danger delete'>Delete</button></td></tr>";  
+        $total = $total + ($value['p_cena'] * $value['p_ilosc']);
+    }
+    $outputTable .= "</table>";
+    $outputTable .= "<div class='text-center'><b>Total: ".$total."</b></div>";
+
+}
+
+echo json_encode($outputTable);
+
